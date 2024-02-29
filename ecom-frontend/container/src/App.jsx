7f28411c3258c13880bb7b6e641212829456421e
Nakul Prasad <nakulprasad10@outlook.com>
2024-02-29 23:45:00 +0530

    Added Header and Footer

--- ecom-frontend/container/src/App.jsx
+++ ecom-frontend/container/src/App.jsx
@@ -1,14 +1,19 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import Header from "./components/Header";
-const ProductRouter = React.lazy(()=>(import("ecom/Productrouter")));
 import "./index.css";
+import Footer from "./components/Footer";
+
+const ProductRouter = React.lazy(() => import("ecom/Productrouter"));
 
 const App = () => (
   <div className="container">
-    <Header/>
+    <Header />
 
-    <Suspense fallback={<div>Loading</div>}><ProductRouter/></Suspense>
+    <Suspense fallback={<div>Loading</div>}>
+      <ProductRouter />
+    </Suspense>
+    <Footer/>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
--- ecom-frontend/container/src/components/Header.js
+++ ecom-frontend/container/src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import '../styles/Header.css'; 
-
-
+import '../styles/Header.css';
 
 const Header = () => {
     return (
-<nav class="navbar navbar-light bg-light">
-   <b>Header Component</b>
-</nav>
+        <div class="container">
+                
+                <header class="d-flex navbar-light py-3">
+                    <ul class="nav nav-pills">
+                        <li class="nav-item"><a href="#" class="nav-link active">Home</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+                    </ul>
+                </header>
+            </div>
     );
 };
 
